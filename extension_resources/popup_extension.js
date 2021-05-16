11cff5ebe74ab00eae76c42c97d94c79fd306d6c
CodingKid124 <sebastianweb231@gmail.com>
2021-05-17 09:24:44 +1200

    add localstorage to checkbox

--- extension_resources/popup_extension.js
+++ extension_resources/popup_extension.js
@@ -4,9 +4,22 @@ document.getElementById('switchforextension').addEventListener('click', function
     console.log(ischeckboxchecked)
     console.log(x.checked)
     if (x.checked == true) {
+        localStorage.setItem("stateOfExtensionCheckbox", ischeckboxchecked);
         console.log("The if statement says the checkbox is checked");
     }
     else {
+        localStorage.setItem("stateOfExtensionCheckbox", ischeckboxchecked);
         console.log("The if statement says the checkbox is not checked");
     }
 });
+
+window.onload = function() {
+    var y = document.getElementById('switchforextension');
+    var localstoragepull = localStorage.getItem("stateOfExtensionCheckbox")
+    if (localstoragepull == "true") {
+        y.checked = true;
+    }
+    else {
+        y.checked = false;
+    }
+};
